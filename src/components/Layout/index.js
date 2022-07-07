eb3ed4bcb11cd8ff65eb747ee3449dbfc7541819
zhaza <Khail.ballin12@yahoo.com>
2022-07-07 00:12:00 -0700

    Finish sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,14 @@
 import Sidebar from "../Sidebar";
 import "./index.scss";
+
 const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+      </div>
+    </div>
   );
 };
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 import { Link, NavLink } from "react-router-dom";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
   return (
@@ -19,14 +20,22 @@ const Sidebar = () => {
         <NavLink activeclassname="active" className="about-link" to="/about">
           <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
+        <NavLink activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/mikhail-jackson12/">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/zhaza/">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
