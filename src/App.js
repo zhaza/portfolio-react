f44873c2a689b381ccc9bd8ec484ffbd98e5f08f
zhaza <Khail.ballin12@yahoo.com>
2022-07-11 22:33:55 -0700

    Add Contact page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
-// import Contact from './components/Contact'
+import Contact from './components/Contact'
 import Layout from './components/Layout'
 // import Portfolio from './components/Portfolio'
 // import Dashboard from './components/Dashboard'
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/contact" element={<Contact />} />
           {/* <Route path="/portfolio" element={<Portfolio />} /> */}
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Route>
