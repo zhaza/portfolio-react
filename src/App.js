eeaa3146e831129fc30dcc53b3cb50d2ea2e92f3
zhaza <Khail.ballin12@yahoo.com>
2022-08-21 23:43:55 -0700

    Last test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -10,14 +10,14 @@ import './App.scss'
 function App() {
   return (
     <>
-      <Switch>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="portfolio" element={<Portfolio />} />
         </Route>
-      </Switch>
+      </Routes>
     </>
   )
 }
